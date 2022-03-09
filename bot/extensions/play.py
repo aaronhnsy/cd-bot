8e113d5db8be7eda8bcf01d2a914a83a512ef412
Axel <axelancerr@gmail.com>
2022-03-09 22:12:12 +0000

    update play extension

--- bot/extensions/play.py
+++ bot/extensions/play.py
@@ -33,6 +33,8 @@ class Play(commands.Cog):
 
         return True
 
+    # Utilities
+
     @staticmethod
     async def _ensure_connected(ctx: custom.Context) -> None:
 
@@ -41,15 +43,14 @@ class Play(commands.Cog):
         author_voice_channel = ctx.author.voice and ctx.author.voice.channel
         bot_voice_channel = ctx.voice_client and ctx.voice_client.voice_channel
 
-        if not author_voice_channel:
-            if bot_voice_channel:
-                raise exceptions.EmbedError(description=f"you must be connected to {bot_voice_channel.mention} to use this command.")
-            raise exceptions.EmbedError(description="you must be connected to a voice channel to use this command.")
+        if (author_voice_channel and bot_voice_channel) and (author_voice_channel == bot_voice_channel):
+            return
 
-        if bot_voice_channel:
-            if bot_voice_channel == author_voice_channel:
-                return
+        if (not author_voice_channel and bot_voice_channel) or (author_voice_channel and bot_voice_channel):
             raise exceptions.EmbedError(description=f"you must be connected to {bot_voice_channel.mention} to use this command.")
+
+        if not author_voice_channel:
+            raise exceptions.EmbedError(description="you must be connected to a voice channel to use this command.")
 
         await author_voice_channel.connect(cls=custom.Player(text_channel=ctx.channel))  # type: ignore
         ctx.voice_client.text_channel = ctx.channel  # type: ignore
@@ -59,57 +60,66 @@ class Play(commands.Cog):
     @commands.command(name="play", aliases=["p"])
     async def play(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Adds a track to the queue.
+        Searches for and adds a track to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
+
+        **Note:**
+        This command supports all sources providing you use a direct URL. When using a search query with this command you will always get a YouTube track.
         """
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE, ctx=ctx)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE, ctx=ctx)
 
     @commands.command(name="play-next", aliases=["play_next", "playnext", "pne"])
     async def play_next(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Adds a track to the start of the queue.
+        Searches for and adds a track to the start of the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
+
+        **Note:**
+        This command supports all sources providing you use a direct URL. When using a search query with this command you will always get a YouTube track.
         """
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE, ctx=ctx, play_next=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE, ctx=ctx, play_next=True)
 
     @commands.command(name="play-now", aliases=["play_now", "playnow", "pno"])
     async def play_now(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Skips the current track and plays the specified track.
+        Searches for and plays a track immediately.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
+
+        **Note:**
+        This command supports all sources providing you use a direct URL. When using a search query with this command you will always get a YouTube track.
         """
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE, ctx=ctx, play_now=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE, ctx=ctx, play_now=True)
 
     # Search
 
-    @commands.command(name="search")
+    @commands.command(name="search", aliases=["s"])
     async def search(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Allows you to select a track from a search result.
+        Allows you to select which tracks to add to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
+
+        **Note:**
+        This command supports all sources providing you use a direct URL. When using a search query with this command you will always get a YouTube track.
         """
 
         await self._ensure_connected(ctx)
@@ -138,12 +148,11 @@ class Play(commands.Cog):
     @commands.command(name="youtube", aliases=["yt"])
     async def youtube(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Adds a track from Youtube to the queue.
+        Searches for tracks from YouTube to add to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
         """
-
         await self.play(ctx, query=query)
 
     @commands.command(name="youtube-next", aliases=["youtube_next", "youtubenext", "ytne"], hidden=True)
@@ -154,17 +163,16 @@ class Play(commands.Cog):
     async def youtube_now(self, ctx: custom.Context, *, query: str) -> None:
         await self.play_now(ctx, query=query)
 
-    # Youtube search
+    # YouTube search
 
     @commands.command(name="youtube-search", aliases=["youtube_search", "youtubesearch", "yts"])
     async def youtube_search(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Allows you to select a track from a Youtube search result.
+        Allows you to select which tracks from YouTube to add to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
         """
-
         await self.search(ctx, query=query)
 
     @commands.command(name="youtube-search-next", aliases=["youtube_search_next", "youtubesearchnext", "ytsne"], hidden=True)
@@ -175,50 +183,47 @@ class Play(commands.Cog):
     async def youtube_search_now(self, ctx: custom.Context, *, query: str) -> None:
         await self.search_now(ctx, query=query)
 
-    # Youtube music
+    # YouTube Music
 
     @commands.command(name="youtube-music", aliases=["youtube_music", "youtubemusic", "ytm"])
     async def youtube_music(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Adds a track from Youtube Music to the queue.
+        Searches for tracks from YouTube Music to add to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
         """
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE_MUSIC, ctx=ctx)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE_MUSIC, ctx=ctx)
 
     @commands.command(name="youtube-music-next", aliases=["youtube_music_next", "youtubemusicnext", "ytmne"], hidden=True)
     async def youtube_music_next(self, ctx: custom.Context, *, query: str) -> None:
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE_MUSIC, ctx=ctx, play_next=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE_MUSIC, ctx=ctx, play_next=True)
 
     @commands.command(name="youtube-music-now", aliases=["youtube_music_now", "youtubemusicnow", "ytmno"], hidden=True)
     async def youtube_music_now(self, ctx: custom.Context, *, query: str) -> None:
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE_MUSIC, ctx=ctx, play_now=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.YOUTUBE_MUSIC, ctx=ctx, play_now=True)
 
-    # Youtube music search
+    # YouTube Music search
 
     @commands.command(name="youtube-music-search", aliases=["youtube_music_search", "youtubemusicsearch", "ytms"])
     async def youtube_music_search(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Allows you to select a track from a Youtube Music search result.
+        Allows you to select which tracks from YouTube Music to add to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
         """
 
         await self._ensure_connected(ctx)
@@ -247,45 +252,42 @@ class Play(commands.Cog):
     @commands.command(name="soundcloud", aliases=["sc"])
     async def soundcloud(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Adds a track from Soundcloud to the queue.
+        Searches for tracks from Soundcloud to add to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
         """
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.SOUNDCLOUD, ctx=ctx)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.SOUNDCLOUD, ctx=ctx)
 
     @commands.command(name="soundcloud-next", aliases=["soundcloud_next", "soundcloudnext", "scne"], hidden=True)
     async def soundcloud_next(self, ctx: custom.Context, *, query: str) -> None:
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.SOUNDCLOUD, ctx=ctx, play_next=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.SOUNDCLOUD, ctx=ctx, play_next=True)
 
     @commands.command(name="soundcloud-now", aliases=["soundcloud_now", "soundcloudnow", "scno"], hidden=True)
     async def soundcloud_now(self, ctx: custom.Context, *, query: str) -> None:
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.SOUNDCLOUD, ctx=ctx, play_now=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.SOUNDCLOUD, ctx=ctx, play_now=True)
 
     # Soundcloud search
 
     @commands.command(name="soundcloud-search", aliases=["soundcloud_search", "soundcloudsearch", "scs"])
     async def soundcloud_search(self, ctx: custom.Context, *, query: str) -> None:
         """
-        Allows you to select a track from a Soundcloud search result.
+        Allows you to select which tracks from Soundcloud to add to the queue.
 
         **Arguments:**
-        `query`: Can be a URL, search query or track name.
+        `query`: The track to search for. Can be a URL or search query.
         """
 
         await self._ensure_connected(ctx)
@@ -317,9 +319,8 @@ class Play(commands.Cog):
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.LOCAL, ctx=ctx)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.LOCAL, ctx=ctx)
 
     @commands.command(name="local-next", aliases=["local_next", "localnext", "lne"], hidden=True)
     @commands.is_owner()
@@ -327,9 +328,8 @@ class Play(commands.Cog):
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.LOCAL, ctx=ctx, play_next=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.LOCAL, ctx=ctx, play_next=True)
 
     @commands.command(name="local-now", aliases=["local_now", "localnow", "lno"], hidden=True)
     @commands.is_owner()
@@ -337,9 +337,8 @@ class Play(commands.Cog):
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.LOCAL, ctx=ctx, play_now=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.LOCAL, ctx=ctx, play_now=True)
 
     # HTTP
 
@@ -349,9 +348,8 @@ class Play(commands.Cog):
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.NONE, ctx=ctx)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.NONE, ctx=ctx)
 
     @commands.command(name="http-next", aliases=["http_next", "httpnext", "hne"], hidden=True)
     @commands.is_owner()
@@ -359,9 +357,8 @@ class Play(commands.Cog):
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.NONE, ctx=ctx, play_next=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.NONE, ctx=ctx, play_next=True)
 
     @commands.command(name="http-now", aliases=["http_now", "httpnow", "hno"], hidden=True)
     @commands.is_owner()
@@ -369,6 +366,5 @@ class Play(commands.Cog):
 
         await self._ensure_connected(ctx)
 
-        async with ctx.channel.typing():
-            assert ctx.voice_client is not None
-            await ctx.voice_client.queue_search(query, source=slate.Source.NONE, ctx=ctx, play_now=True)
+        assert ctx.voice_client is not None
+        await ctx.voice_client.queue_search(query, source=slate.Source.NONE, ctx=ctx, play_now=True)
