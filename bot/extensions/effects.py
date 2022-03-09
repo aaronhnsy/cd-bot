3fd8a6e8406e710702603aeda0dc7241f808275e
Axel <axelancerr@gmail.com>
2022-03-09 17:00:10 +0000

    update effects extension

--- bot/extensions/effects.py
+++ bot/extensions/effects.py
@@ -20,7 +20,7 @@ def setup(bot: CD) -> None:
 
 class Effects(commands.Cog):
     """
-    Toggle and control different audio effects.
+    Toggle different audio effects.
     """
 
     def __init__(self, bot: CD) -> None:
@@ -33,204 +33,146 @@ class Effects(commands.Cog):
 
         return True
 
-    # Effects
+    # Control
+
+    EFFECT_MAP: dict[enums.Effect, dict[str, slate.BaseFilter]] = {
+        enums.Effect.ROTATION:  {"rotation": slate.Rotation(rotation_hertz=0.5)},
+        enums.Effect.NIGHTCORE: {"timescale": slate.Timescale(speed=1.12, pitch=1.12)},
+        enums.Effect.MONO:      {"channel_mix": slate.ChannelMix(left_to_right=1, right_to_left=1)},
+        enums.Effect.LEFT_EAR:  {"channel_mix": slate.ChannelMix(right_to_right=0, right_to_left=1)},
+        enums.Effect.RIGHT_EAR: {"channel_mix": slate.ChannelMix(left_to_left=0, left_to_right=1)},
+    }
+
+    INVERSE_EFFECT_MAP: dict[enums.Effect, dict[str, slate.BaseFilter]] = {
+        enums.Effect.ROTATION:  {"rotation": slate.Rotation()},
+        enums.Effect.NIGHTCORE: {"timescale": slate.Timescale()},
+        enums.Effect.MONO:      {"channel_mix": slate.ChannelMix()},
+        enums.Effect.LEFT_EAR:  {"channel_mix": slate.ChannelMix()},
+        enums.Effect.RIGHT_EAR: {"channel_mix": slate.ChannelMix()},
+    }
+
+    INCOMPATIBLE_EFFECTS: dict[enums.Effect, list[enums.Effect]] = {
+        enums.Effect.MONO:      [enums.Effect.LEFT_EAR, enums.Effect.RIGHT_EAR],
+        enums.Effect.LEFT_EAR:  [enums.Effect.MONO, enums.Effect.RIGHT_EAR],
+        enums.Effect.RIGHT_EAR: [enums.Effect.MONO, enums.Effect.LEFT_EAR],
+    }
+
+    async def _toggle_effect(self, ctx: custom.Context, effect: enums.Effect) -> None:
+
+        assert ctx.voice_client
+
+        if effect in ctx.voice_client.effects:
+            description = f"**disabled** the **{effect.value}** audio effect."
+            ctx.voice_client.effects.remove(effect)
+            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, **self.INVERSE_EFFECT_MAP[effect]))
+
+        else:
+            description = f"**enabled** the **{effect.value}** audio effect."
+            ctx.voice_client.effects.add(effect)
+            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, **self.EFFECT_MAP[effect]))
+
+            if effect in self.INCOMPATIBLE_EFFECTS:
+                for incompatible_effect in self.INCOMPATIBLE_EFFECTS[effect]:
+                    try:
+                        ctx.voice_client.effects.remove(incompatible_effect)
+                    except KeyError:
+                        pass
+
+        await ctx.reply(
+            embed=utils.embed(
+                colour=values.GREEN,
+                description=description
+            )
+        )
+
+    @staticmethod
+    async def _reset_effects(ctx: custom.Context) -> None:
+
+        assert ctx.voice_client
+
+        ctx.voice_client.effects.clear()
+        await ctx.voice_client.set_filter(slate.Filter())
+        await ctx.reply(
+            embed=utils.embed(
+                colour=values.GREEN,
+                description="**disabled** all audio effects."
+            )
+        )
+
+    # Commands
 
     @commands.command(name="8d")
     @checks.is_author_connected()
     @checks.is_player_connected()
     async def _8d(self, ctx: custom.Context) -> None:
         """
-        Toggles the 8D audio effect.
+        Toggles an 8D audio effect.
 
-        This effect makes the audio sound like it's rotating around your head.
+        This effect makes the audio sound like its rotating around your head.
         """
 
-        assert ctx.voice_client is not None
-
-        if enums.Effect.ROTATION in ctx.voice_client.effects:
-            ctx.voice_client.effects.remove(enums.Effect.ROTATION)
-            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, rotation=slate.Rotation()))
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**disabled** the **8d** audio effect."
-                )
-            )
-
-        else:
-            ctx.voice_client.effects.add(enums.Effect.ROTATION)
-            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, rotation=slate.Rotation(rotation_hertz=0.5)))
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**enabled** the **8d** audio effect."
-                )
-            )
+        await self._toggle_effect(ctx, enums.Effect.ROTATION)
 
     @commands.command(name="night-core", aliases=["night_core", "nightcore", "nc"])
     @checks.is_author_connected()
     @checks.is_player_connected()
     async def night_core(self, ctx: custom.Context) -> None:
         """
-        Toggles the nightcore audio effect.
+        Toggles a nightcore audio effect.
 
         This effect slightly increases the speed and pitch of the audio.
         """
 
-        assert ctx.voice_client is not None
-
-        if enums.Effect.NIGHTCORE in ctx.voice_client.effects:
-            ctx.voice_client.effects.remove(enums.Effect.NIGHTCORE)
-            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, timescale=slate.Timescale()))
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**disabled** the **nightcore** audio effect."
-                )
-            )
-
-        else:
-            ctx.voice_client.effects.add(enums.Effect.NIGHTCORE)
-            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, timescale=slate.Timescale(speed=1.12, pitch=1.12)))
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**enabled** the **nightcore** audio effect."
-                )
-            )
+        await self._toggle_effect(ctx, enums.Effect.NIGHTCORE)
 
     @commands.command(name="mono")
     @checks.is_author_connected()
     @checks.is_player_connected()
     async def mono(self, ctx: custom.Context) -> None:
         """
-        Toggles the mono audio effect.
+        Toggles a mono audio effect.
 
         This effect makes the left and right audio channels play the same thing.
 
-        **Note:** Enabling this effect will disable the left and right ear effects.
+        **Note:** Enabling this effect will disable the `left-ear` and `right-ear` effects.
         """
 
-        assert ctx.voice_client is not None
+        await self._toggle_effect(ctx, enums.Effect.MONO)
 
-        if enums.Effect.MONO in ctx.voice_client.effects:
-            ctx.voice_client.effects.remove(enums.Effect.MONO)
-            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, channel_mix=slate.ChannelMix()))
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**disabled** the **mono** audio effect."
-                )
-            )
-
-        else:
-
-            ctx.voice_client.effects.add(enums.Effect.MONO)
-            await ctx.voice_client.set_filter(
-                slate.Filter(ctx.voice_client.filter, channel_mix=slate.ChannelMix(left_to_right=1, right_to_left=1))
-            )
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**enabled** the **mono** audio effect."
-                )
-            )
-
-            if enums.Effect.LEFT_EAR in ctx.voice_client.effects:
-                ctx.voice_client.effects.remove(enums.Effect.LEFT_EAR)
-            if enums.Effect.RIGHT_EAR in ctx.voice_client.effects:
-                ctx.voice_client.effects.remove(enums.Effect.RIGHT_EAR)
-
-    @commands.command(name="left-ear", aliases=["left_ear", "leftear", "left", "le"])
+    @commands.command(name="left-ear", aliases=["left_ear", "leftear", "left"])
     @checks.is_author_connected()
     @checks.is_player_connected()
     async def left_ear(self, ctx: custom.Context) -> None:
         """
-        Toggles the left ear audio effect.
+        Toggles a left ear audio effect.
 
         This effect makes the audio only come out of your left headphone/speaker/earbud.
 
-        **Note:** Enabling this effect will disable the mono and right ear effects.
+        **Note:** Enabling this effect will disable the `mono` and `right-ear` effects.
         """
 
-        assert ctx.voice_client is not None
+        await self._toggle_effect(ctx, enums.Effect.LEFT_EAR)
 
-        if enums.Effect.LEFT_EAR in ctx.voice_client.effects:
-            ctx.voice_client.effects.remove(enums.Effect.LEFT_EAR)
-            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, channel_mix=slate.ChannelMix()))
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**disabled** the **left-ear** audio effect."
-                )
-            )
-
-        else:
-
-            ctx.voice_client.effects.add(enums.Effect.LEFT_EAR)
-            await ctx.voice_client.set_filter(
-                slate.Filter(ctx.voice_client.filter, channel_mix=slate.ChannelMix(right_to_right=0, right_to_left=1))
-            )
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**enabled** the **left-ear** audio effect."
-                )
-            )
-
-            if enums.Effect.MONO in ctx.voice_client.effects:
-                ctx.voice_client.effects.remove(enums.Effect.MONO)
-            if enums.Effect.RIGHT_EAR in ctx.voice_client.effects:
-                ctx.voice_client.effects.remove(enums.Effect.RIGHT_EAR)
-
-    @commands.command(name="right-ear", aliases=["right_ear", "rightear", "right", "re"])
+    @commands.command(name="right-ear", aliases=["right_ear", "rightear", "right"])
     @checks.is_author_connected()
     @checks.is_player_connected()
     async def right_ear(self, ctx: custom.Context) -> None:
         """
-        Toggles the right ear audio effect.
+        Toggles a right ear audio effect.
 
         This effect makes the audio only come out of your right headphone/speaker/earbud.
 
-        **Note:** Enabling this effect will disable the mono and left ear effects.
+        **Note:** Enabling this effect will disable the `mono` and `left-ear` effects.
         """
 
-        assert ctx.voice_client is not None
+        await self._toggle_effect(ctx, enums.Effect.RIGHT_EAR)
 
-        if enums.Effect.RIGHT_EAR in ctx.voice_client.effects:
-            ctx.voice_client.effects.remove(enums.Effect.RIGHT_EAR)
-            await ctx.voice_client.set_filter(slate.Filter(ctx.voice_client.filter, channel_mix=slate.ChannelMix()))
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**disabled** the **right-ear** audio effect."
-                )
-            )
-
-        else:
-
-            ctx.voice_client.effects.add(enums.Effect.RIGHT_EAR)
-            await ctx.voice_client.set_filter(
-                slate.Filter(ctx.voice_client.filter, channel_mix=slate.ChannelMix(left_to_left=0, left_to_right=1))
-            )
-            await ctx.reply(
-                embed=utils.embed(
-                    colour=values.GREEN,
-                    description="**enabled** the **right-ear** audio effect."
-                )
-            )
-
-            if enums.Effect.LEFT_EAR in ctx.voice_client.effects:
-                ctx.voice_client.effects.remove(enums.Effect.LEFT_EAR)
-            if enums.Effect.MONO in ctx.voice_client.effects:
-                ctx.voice_client.effects.remove(enums.Effect.MONO)
-
-    @commands.command(name="reset")
+    @commands.command(name="reset-effects", aliases=["reset_effects", "reseteffects"])
     @checks.is_author_connected()
     @checks.is_player_connected()
-    async def reset(self, ctx: custom.Context) -> None:
+    async def reset_effects(self, ctx: custom.Context) -> None:
         """
-        Disables all audio effects.
+        Resets all audio effects.
         """
 
         assert ctx.voice_client is not None
