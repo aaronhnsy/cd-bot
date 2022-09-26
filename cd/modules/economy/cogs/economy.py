462e89dfde83a4ff6fed7d4c4c527914fce91ad0
Axelancerr <axelancerr@gmail.com>
2022-09-26 01:43:37 +0100

    feat: make level command a hybrid command

--- cd/modules/economy/cogs/economy.py
+++ cd/modules/economy/cogs/economy.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Callable
 from typing import TYPE_CHECKING, Literal
 
 import discord
@@ -17,6 +18,10 @@ __all__ = (
 )
 
 
+_MEMBER_DEFAULT: Callable[[custom.Context], discord.User | discord.Member] = lambda ctx: ctx.author
+_MEMBER_CONVERTER = commands.parameter(default=_MEMBER_DEFAULT)
+
+
 class Economy(commands.Cog):
 
     def __init__(self, bot: CD) -> None:
@@ -29,14 +34,14 @@ class Economy(commands.Cog):
 
         return True
 
-    @commands.command(name="level", aliases=["lvl"])
-    async def level(self, ctx: custom.Context, person: discord.Member | None = None) -> None:
-
-        if not person:
-            assert isinstance(ctx.author, discord.Member)
-            person = ctx.author
+    @commands.hybrid_command(name="level", aliases=["lvl"])
+    async def level(self, ctx: custom.Context, person: discord.Member = _MEMBER_CONVERTER) -> None:
+        """
+        Shows yours, or another member's level card.
+        """
 
         assert ctx.guild is not None
-        url = await self.bot.manager.create_level_card(guild_id=ctx.guild.id, user_id=person.id)
 
-        await ctx.send(url)
+        async with ctx.typing():
+            url = await self.bot.manager.create_level_card(guild_id=ctx.guild.id, user_id=person.id)
+            await ctx.send(url)
