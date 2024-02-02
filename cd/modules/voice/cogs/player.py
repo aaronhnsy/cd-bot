0cde2477932f0c9a370c8fc8527c461b87f7f783
aaronhnsy <aaronhnsy@gmail.com>
2024-02-02 03:31:44 +0000

    add some convenience commands while working on discord-ext-lava

--- cd/modules/voice/cogs/player.py
+++ cd/modules/voice/cogs/player.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 # Project
 from cd import custom
+from ..custom import Player
 
 
 __all__ = ["VoicePlayer"]
@@ -10,6 +11,10 @@ __all__ = ["VoicePlayer"]
 
 class VoicePlayer(custom.Cog):
 
-    @commands.command(name="play", aliases=["p", "pl", "ply"])
-    async def play(self, ctx: custom.Context) -> None:
-        await ctx.reply("play")
+    @commands.command(name="join", aliases=["connect"])
+    async def join(self, ctx: custom.Context) -> None:
+        await ctx.author.voice.channel.connect(cls=Player(link=self.bot.lavalink))
+
+    @commands.command(name="play")
+    async def play(self, ctx: custom.Context, *, search: str) -> None:
+        await ctx.voice_client.update(track=(await self.bot.lavalink.search(f"ytsearch:{search}")).tracks[0])
